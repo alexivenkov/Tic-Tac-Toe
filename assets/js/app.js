a908a726ef7125baed2bc036021d4919bde19db7
Alexander Ivenkov <Aleksandr.Ivenkov@noveogroup.com>
2019-04-07 00:52:03 +0300

    set up frontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,46 @@
 require('./bootstrap');
 
+const AVAILABLE_UNITS = [
+    'X', 'O'
+];
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        showUnitSelector: true,
+        showBoard: false,
+        playerUnit: null,
+        board: [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ]
+    },
+    methods: {
+        chooseUnit: function (unit) {
+            if (!AVAILABLE_UNITS.includes(unit)) {
+                alert('Selected unit ' + unit + ' is unavailable');
+
+                return false;
+            }
+
+            this.playerUnit = unit;
+            this.showUnitSelector = false;
+            this.showBoard = true;
+        },
+        makeMove: function (row, cell) {
+            const newRow = this.board[row].slice(0);
+            newRow[cell] = this.playerUnit;
+            this.$set(this.board, row, newRow);
+        },
+        restartGame: function () {
+            this.showBoard = false;
+            this.showUnitSelector = true;
+            this.board = [
+                ['', '', ''],
+                ['', '', ''],
+                ['', '', '']
+            ];
+        }
+    }
 });
